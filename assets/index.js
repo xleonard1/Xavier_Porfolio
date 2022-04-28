17f6d1f58bb18b97c6d02c2ec19f257de3a1159d
xleonard1 <xavier.lnrd@gmail.com>
2022-04-28 13:00:05 -0400

    added comments to javascript

--- assets/index.js
+++ assets/index.js
@@ -124,7 +124,6 @@ closeModal.addEventListener('click', function(event){
 })
 
 window.addEventListener('click', function(event){
-    
     if(event.target == modal) {
         modal.style.display = 'none'
     }
